bddfaf5d3bcb73eaeeafe2e23d4644ba8ace3182
IACZ <alvaro.crespo@gmail.com>
2022-01-14 13:48:13 +0100

    Update 1.Document-list-all-regions-for-ec2.py

--- 6.Usage of Meta object/1.Document-list-all-regions-for-ec2.py
+++ 6.Usage of Meta object/1.Document-list-all-regions-for-ec2.py
@@ -1,3 +1,6 @@
+'''
+meta allows to use operations form the client with the resource
+'''
 import boto3
 aws_mag_con=boto3.session.Session(profile_name="root")
 ec2_con_re=aws_mag_con.resource(service_name="ec2")
